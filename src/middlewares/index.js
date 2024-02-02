a9d452532efea55a305c2d90241b5fe7ab4c06de
mobintahmasbi <mobintahmasbi13801202@gmail.com>
2024-02-02 21:01:28 +0330

    add body parser to my project

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import path from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,4 +10,6 @@ const __dirname = path.dirname(__filename);
 
 export default (app) => {
     app.use(express.static(path.join(__dirname, "../../public")))
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
 }
